9bdf66661430b5d2fa382d607ba432daeb8ce5dc
rushi-smb <rushi@smbios.co.in>
2023-02-11 14:19:58 +0530

    Scaffolding Controllers and Actions

--- Data/HotelListingDbContext.cs
+++ Data/HotelListingDbContext.cs
@@ -10,11 +10,14 @@ namespace UdemyAPIPractice.Data
         }
 
         // To Generate Auto Created Database and Tables Hotels & Countries we structured in Hotel and Country Class/Entity respectively
+        // and then we can add-migration
+
         public DbSet<Hotel> Hotels { get; set; }
         public DbSet<Country> Countries { get; set; }
 
         // Seeding is the act of putting default data
         // So what we need to do is define in our DB context that we want to override
+        // Write down this override method and add-migration SeededCountriesAndHotel will put this data in Country and Hotel table
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
